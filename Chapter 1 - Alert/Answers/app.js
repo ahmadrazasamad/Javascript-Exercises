35371c81fca20f5089ba588281d64785758a5036
Ahmad Raza <ahmadrazasamad@gmail.com>
2024-02-10 16:29:03 +0500

    Chapter 1 completed

---
+++ Chapter 1 - Alert/Answers/app.js
@@ -0,0 +1,18 @@
+// 1. Write a script to greet your website visitor using JS alert box.
+alert("Hello, welcome to our website.");
+
+// 2. Write a script to display following message on your web page:
+alert("Error! Please enter a valid password.");
+
+// 3. Write a script to display following message on your web page:
+alert("Welcome to JS Land...\nHappy Coding.");
+
+// 4. Write a script to display following messages in sequence:
+alert("Welcome to JS Land...");
+alert("Happy Coding.");
+
+// 5. Generate the following message through browser’s developer console:
+alert("Hello... I can run JS through my web browser's console."); // to be pasted in browser's console
+
+// 6. Make use of alerts in your new/existing HTML & CSS project.
+alert("Usage of alert in existing html project");
